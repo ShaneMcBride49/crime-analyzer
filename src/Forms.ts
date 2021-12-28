d50caf955c69a9a27b0bec32b40a28b4d34d5191
Shane McBride <captainchaos4999@gmail.com>
2021-12-28 09:43:31 -0600

    Drag and drop system 1

---
+++ src/Forms.ts
@@ -0,0 +1,60 @@
+export type Input = {
+    type: 'input',
+    name: string,
+} | {
+    type: 'select',
+    options: string[],
+    name: string
+}
+export type Form = {
+    name: string,
+    inputs: Input[]
+}
+function single(name: string): Input[] {
+    return [{ type: 'input', name }];
+}
+
+const Forms: Form[] = [
+    {
+        name: "Average",
+        inputs: single('Attribute')
+    },
+    {
+        name: "Pluck",
+        inputs: single('Attribute')
+    },
+    {
+        name: "Sum",
+        inputs: single('Attribute')
+    },
+    {
+        name: "Limit",
+        inputs: single('Count')
+    },
+    {   name: "Skip",
+        inputs: single('Count')
+    },
+    {
+        name: "Distinct",
+        inputs: single('Attribute')
+    },
+    {
+        name: "Filter",
+        inputs: [
+            {
+                name: 'Attribute',
+                type: 'input'
+            },
+            {
+                name: 'Operation',
+                type: 'select',
+                options: ['==', '!=']
+            },
+            {
+                name: 'Value',
+                type: 'input'
+            },
+        ]
+    }
+];
+export default Forms;
