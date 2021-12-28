f73bf768757fc92fd7453947fa3aab223f0a25ba
Shane McBride <shanetmcbride@gmail.com>
2021-12-28 10:01:04 -0600

    Drag and drop 2

---
+++ src/Sidebar.tsx
@@ -0,0 +1,32 @@
+import React, { DragEvent } from 'react';
+import Forms from "./Forms";
+
+const onDragStart = (event: DragEvent, nodeType: string) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+};
+
+const Sidebar = () => {
+    const formsArray = Forms.map((form)=>
+        <div className="react-flow__node-input" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
+            {form.name}
+        </div>
+    );
+    return (
+        <aside>
+            <div className="description"><h1>Nodes</h1></div>
+            <div className="react-flow__node-input" onDragStart={(event: DragEvent) => onDragStart(event, 'input')} draggable>
+                Input Node
+            </div>
+            {formsArray}
+            {/*<div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>*/}
+            {/*    Default Node*/}
+            {/*</div>*/}
+            <div className="react-flow__node-output" onDragStart={(event: DragEvent) => onDragStart(event, 'output')} draggable>
+                Output Node
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DnDFlow from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <div className="window">
+        <DnDFlow/>
+      </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
